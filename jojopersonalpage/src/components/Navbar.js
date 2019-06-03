91229c40cf31d4a6cb6f38da68a459993a9cff63
jonelle <jonellebain@pursuit.org>
2019-06-03 14:01:27 -0400

    fixing navbar

--- jojopersonalpage/src/components/Navbar.js
+++ jojopersonalpage/src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../css/Navbar.css";
+import logo from "../photos/personalLogo.png";
 
 const Navbar = () => {
   return (
     <div className="navbar">
+      <span className="nav_logo">
+        <Link to={"/"}>
+          <img className="logo" src={logo} alt="" width="120px" height="auto" />
+        </Link>
+      </span>
+
       <div className="links">
-        <Link to={"/"}>Home</Link>
         <Link to="/https://www.linkedin.com/in/jonelle-bain/">LinkedIn</Link>
         <Link to="/https://github.com/jbain424/">GitHub</Link>
         <Link to="/resume">Resume</Link>
